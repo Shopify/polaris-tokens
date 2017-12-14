4b9e4fa13dcd526888ff2d92bd83bee7570ad06a
Kaelig <kaelig.deloumeauprigent@shopify.com>
2017-12-14 12:31:50 -0500

    Add snapshot testing for colors

---
+++ __tests__/colors.test.js
@@ -0,0 +1,24 @@
+/* eslint-env jest */
+
+const fs = require('fs');
+const path = require('path');
+
+const colorFiles = [
+  'colors.custom.map.scss',
+  'colors.custom-properties.css',
+  'colors.json',
+  'colors.scss',
+  'Polaris.sketchpalette',
+];
+
+colorFiles.map(filename =>
+  it(`renders ${filename} correctly`, () => {
+    const contents = fs.readFileSync(
+      path.join(__dirname, '..', 'dist', filename),
+      {
+        encoding: 'utf-8',
+      },
+    );
+    expect(contents).toMatchSnapshot();
+  }),
+);
