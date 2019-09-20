68772b33947692eaa1113218e3363e4cf5720361
Kaelig <kaelig.deloumeauprigent@shopify.com>
2019-09-20 10:26:15 -0700

    Add test ensuring contributors are releasing beta versions

--- tests/package.json.test.js
+++ tests/package.json.test.js
@@ -2,4 +2,7 @@ describe('package.json', () => {
   it('doesn’t have any dependencies', () => {
     expect(require('../package.json').dependencies).toBeUndefined();
   });
+  it('has “beta” in the version (delete test before merging to master)', () => {
+    expect(require('../package.json').version).toContain('beta');
+  });
 });
