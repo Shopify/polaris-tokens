e4f40ba63dc133e550747514d515581ee820faa1
Marten Bjork <marten.bjork@shopify.com>
2020-03-19 14:54:46 -0400

    Update wording

--- src/configs/base.ts
+++ src/configs/base.ts
@@ -310,7 +310,7 @@ export const config: Config = {
     {
       name: 'textDisabled',
       description:
-        'For use as a disabled text color and as placeholder text in text fields.',
+        'For use as a disabled text color and as a placeholder text color in text fields.',
       light: {lightness: 60},
       dark: {lightness: 48.2},
       meta: {
