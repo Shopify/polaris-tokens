26605c1800b2c52eb9c65ff914653d5924281bc0
Kaelig <kaelig.deloumeauprigent@shopify.com>
2020-12-18 11:00:30 -0800

    Upgrade node to the latest LTS version

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.colorFactory = void 0;
 var color_factory_1 = require("./color-factory");
 Object.defineProperty(exports, "colorFactory", { enumerable: true, get: function () { return color_factory_1.colorFactory; } });
