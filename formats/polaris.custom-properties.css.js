9ad345bd998234226c8aea0b13331119c5755505
Kaelig <kaelig.deloumeauprigent@shopify.com>
2021-01-26 18:09:24 -0800

    Add support for .hbs templates

--- formats/polaris.custom-properties.css.js
+++
@@ -1,8 +0,0 @@
-module.exports = `:root {
-  {{#each props as |prop|}}
-    {{#if prop.comment}}
-    {{{trimLeft (indent (comment (trim prop.comment)))}}}
-    {{/if}}
-    --p-{{kebabcase prop.name}}: {{#eq prop.type "string"}}"{{/eq}}{{{prop.value}}}{{#eq prop.type "string"}}"{{/eq}};
-  {{/each}}
-}`;
